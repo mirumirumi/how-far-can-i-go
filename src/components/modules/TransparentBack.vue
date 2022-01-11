096759e6a67e1bd2d7232c13fff2a05bc88afc0e
mirumi <info@milmemo.net>
2022-01-11 23:49:09 +0900

    [add] : teleport modal back component

---
+++ src/components/modules/TransparentBack.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="transparent_back"></div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+.transparent_back {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 13;
+  display: block;
+  cursor: default;
+  content: " ";
+  background: transparent;
+}
+</style>
